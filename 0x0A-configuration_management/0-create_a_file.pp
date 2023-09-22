48c3caf1c0b3a4d00ef535cebe4ba7a87e2cc0d5
Peacecole <onyangocollince@gmail.com>
2023-09-22 20:54:54 +0000

    Task 0 - Using Puppet, create a file in /tmp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,6 +1,6 @@
 #Using Puppet, create a file in /tmp
 file { '/tmp/school':
-  ensure  =>  'file',
+  ensure  =>  present,
   content =>  'I love Puppet',
   mode 	  =>  '0744',
   owner   =>  'www-data',
