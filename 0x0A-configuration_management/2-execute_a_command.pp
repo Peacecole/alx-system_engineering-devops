9b390d6d4ddce36727b71d720db38f23e1609cba
Peacecole <onyangocollince@gmail.com>
2023-09-22 22:26:58 +0000

    Task 2 - Using Puppet, create a manifest that kills a process named killmenow

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -2,6 +2,6 @@
 #Use the exec Puppet resource
 #Use pkill
 exec { 'killmenow':
-  command     => 'pkill killmenow',    #kill command
-  path        => ['/bin', '/usr/bin'], # Specify the command's path as needed
+  command  => 'pkill killmenow',    #executes the kill command pkill on killmenow
+  path     => ['/bin', '/usr/bin'], # Specify the command's path as needed
 }
