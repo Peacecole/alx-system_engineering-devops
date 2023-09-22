6df8fd51e7fcdd08e765e690cb4d7aea1c6040da
Peacecole <onyangocollince@gmail.com>
2023-09-22 21:36:41 +0000

    Task 1 - Using Puppet, install flask from pip3

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,7 @@
 #Using Puppet, install flask from pip3
 #flask is the package to be installed
 #2.1.0 is the version of flask being installed
-#pip3 is the source of flask being installed
+#pip3 is the provider of flask being installed
 
 package { 'flask':
   ensure  =>  '2.1.0',
