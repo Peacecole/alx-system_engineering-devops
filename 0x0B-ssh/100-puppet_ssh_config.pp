83c5c367d1c0acc22bfbe8c6061ea3f7ff7027a0
Peacecole <onyangocollince@gmail.com>
2023-09-26 16:30:22 +0000

    Task 4 - SSH configuration file so that you can connect to a server without typing a password

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -4,13 +4,11 @@
 file { '/etc/ssh/ssh_config':
   ensure => present,			#configuration file available
 }
-
 file_line { 'Turn off password auth':
   path  => '/etc/ssh/ssh_config', 	#path to config file
   line  => 'PasswordAuthentication no', #command to password authentication
   match => '^#PasswordAuthentication',  #command to password authenticatio
 }
-
 file_line { 'Declare identity file':
   path  => '/etc/ssh/ssh_config',	 #path to config file
   lien  => 'IdentifyFile ~/.ssh/school', #location of the key file
