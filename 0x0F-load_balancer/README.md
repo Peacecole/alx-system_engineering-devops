81bbdc81e56e0fa00f7e72722cb49d669ae7569d
Peacecole <onyangocollince@gmail.com>
2023-10-02 20:59:25 +0000

    The readme file

---
+++ 0x0F-load_balancer/README.md
@@ -0,0 +1,5 @@
+This directory is about the 0x0F. Load balancer project
+It contains the following files
+
+	- 0-custom_http_response_header
+	- 1-install_load_balancer
--- 0x10-python-network_0/0-body_size.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Takes in and sends a request to a given URL then displays the size of the body of the response
-curl -sI $1 | grep "Content-Length" | cut -d " " -f2
